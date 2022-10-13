8804e92e129446122fd1245f7f6d5f240a9c4cde
xmu-csnoob <395110618@qq.com>
2022-10-13 20:35:15 +0800

    #2022/10/13 完善TopRecordPo的构造函数

--- src/main/java/cn/edu/xmu/wwf/sinatop/sinatops/model/TopRecordPo.java
+++ src/main/java/cn/edu/xmu/wwf/sinatop/sinatops/model/TopRecordPo.java
@@ -15,6 +15,10 @@ public class TopRecordPo {
     double popularity;
     String platform;
     public TopRecordPo(String title,int ranking,String time,double popularity,String platform){
-
+        this.title=title;
+        this.ranking=ranking;
+        this.time=time;
+        this.popularity=popularity;
+        this.platform=platform;
     }
 }
